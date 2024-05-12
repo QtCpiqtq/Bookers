6e102f12d047505ef63a0c34fcb671820a3a6421
HideTsutsumi <167219646+QtCpiqtq@users.noreply.github.com>
2024-05-12 13:35:33 +0000

    [finish]showページ

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'books/new' => 'books#new'
   post 'books' => 'books#create'
   get 'books/index' => 'books#index'
+  get 'books/:id' => 'books#show', as: 'bookshow'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
