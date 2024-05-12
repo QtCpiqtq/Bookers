67eea3f4d9ff0cf36bbd2729d265277b7ce874f8
HideTsutsumi <167219646+QtCpiqtq@users.noreply.github.com>
2024-05-12 14:19:51 +0000

    [finish]editページ

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   get 'books/index' => 'books#index'
   get 'books/:id' => 'books#show', as: 'bookshow'
+  get 'books/:id/edit' => 'books#edit', as: 'bookedit'
+  patch 'books/:id' => 'books#update', as: 'bookpatch'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
