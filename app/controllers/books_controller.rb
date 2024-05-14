8ca80553626db15fe2e3663d2b5fc9c224740475
HideTsutsumi <167219646+QtCpiqtq@users.noreply.github.com>
2024-05-14 08:32:11 +0000

    [update]新規投稿フォームをindexページ内に追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,19 @@
 class BooksController < ApplicationController
-  def new
-      @book = Book.new
+  def index
+        @books = Book.all
+        @book = Book.new
   end
   
   def create
       @book = Book.new(book_params)
       if @book.save
       flash[:notice] = "Book was saccessfully created"
-      redirect_to bookshow_path(@book.id)
+      redirect_to book_path(@book.id)
       else
-      render :new
+      render :index
       end
   end
-
-  def index
-      @book = Book.all
-  end
-
+  
   def show
       @book = Book.find(params[:id])
   end
@@ -29,7 +26,7 @@ class BooksController < ApplicationController
       @book = Book.find(params[:id])
       if  @book.update(book_params)
       flash[:notice] = "Book was saccessfully updated"
-      redirect_to bookshow_path(@book.id)
+      redirect_to book_path(@book.id)
       else
       render :edit
       end
@@ -38,7 +35,7 @@ class BooksController < ApplicationController
   def destroy
       book = Book.find(params[:id])
       book.destroy
-      redirect_to '/books/index'
+      redirect_to '/books'
   end
   
   private 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'books/new' => 'books#new'
-  post 'books' => 'books#create'
-  get 'books/index' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'bookshow'
-  get 'books/:id/edit' => 'books#edit', as: 'bookedit'
-  patch 'books/:id' => 'books#update', as: 'bookpatch'
-  delete 'books/:id' => 'books#destroy', as: 'bookdelete'
+  get 'homes/top' => "homes#top"
+  resources :books 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
